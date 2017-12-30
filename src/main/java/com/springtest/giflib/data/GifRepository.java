56dabaf602f26a5fca9a3bdfc42748b8f1f67685
Kashif Abro <Kash@Kashifs-MacBook-Pro.local>
2017-12-30 18:50:31 +0000

    category and favourites

--- src/main/java/com/springtest/giflib/data/GifRepository.java
+++ src/main/java/com/springtest/giflib/data/GifRepository.java
@@ -4,6 +4,7 @@ import com.springtest.giflib.model.Gif;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,12 +12,12 @@ import java.util.List;
 public class GifRepository {
 
     private static final List<Gif> ALL_GIFS = Arrays.asList(
-        new Gif("android-explosion", LocalDate.of(2015,2,13), "Kash of Bedfont", false),
-        new Gif("ben-and-mike", LocalDate.of(2015,10,30), "SlutJah", true),
-        new Gif("book-dominos", LocalDate.of(2015,9,15), "Jaxxx", false),
-        new Gif("compiler-bot", LocalDate.of(2015,2,13), "Mokta", true),
-        new Gif("cowboy-coder", LocalDate.of(2015,2,13), "Muaqet", false),
-        new Gif("infinite-andrew", LocalDate.of(2015,8,23), "Muqta", true)
+        new Gif("android-explosion", 1,LocalDate.of(2015,2,13), "Kash of Bedfont", false),
+        new Gif("ben-and-mike", 2,LocalDate.of(2015,10,30), "SlutJah", true),
+        new Gif("book-dominos", 3,LocalDate.of(2015,9,15), "Jaxxx", false),
+        new Gif("compiler-bot", 3,LocalDate.of(2015,2,13), "Mokta", true),
+        new Gif("cowboy-coder", 2,LocalDate.of(2015,2,13), "Muaqet", false),
+        new Gif("infinite-andrew", 1,LocalDate.of(2015,8,23), "Muqta", true)
 
     );
 
@@ -27,6 +28,27 @@ public class GifRepository {
             }
         }
         return null;
+    }
+
+    public List<Gif> findByCategoryId(int id){
+        List<Gif> gifs = new ArrayList<>();
+        for (Gif gif : ALL_GIFS) {
+            if(gif.getCategoryId() == id){
+                gifs.add(gif);
+            }
+
+        }
+        return gifs;
+    }
+
+    public List<Gif> getAllFav(){
+        List<Gif> gifs = new ArrayList<>();
+        for (Gif gif : ALL_GIFS){
+            if(gif.isFavorite() == true){
+                gifs.add(gif);
+            }
+        }
+        return gifs;
     }
 
     public List<Gif> getAllGifs(){
